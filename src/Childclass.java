44613b582efe1c5e312f13652b9e5c90f208aab0
Shubham <Shubham@192.168.0.9>
2019-11-24 18:57:36 +0530

    updaed new code

---
+++ src/Childclass.java
@@ -0,0 +1,10 @@
+
+public class Childclass extends Student6 {
+
+	Childclass(int i, String n) {
+		super(i, n);
+	}
+	
+	
+	}
+
---
+++ src/Student.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Student {
+		int rollno;
+		String name;
+		int age;
+		Student(int rollno,String name,int age){
+			this.rollno=rollno;
+			this.name=name;
+			this.age=age;
+		}
+		
+		}
+
+	
